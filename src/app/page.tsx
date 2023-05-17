ab1902068243e70eea4a5997d6a1764c039bc614
Andre Lino <andre.cesar97@hotmail.com>
2023-05-17 08:53:27 -0300

    ajustes de configuracao de fonte

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,8 @@
 export default function Home() {
   return (
-   <h1>hello world</h1>
+    <div className="h-screen bg-zinc-950 p-6 text-zinc-50">
+      <h1 className="text-4xl font-bold">Sua capsula do tempo</h1>
+      <h1 className="font-alt text-4xl font-bold">Sua capsula do tempo</h1>
+    </div>
   )
 }
