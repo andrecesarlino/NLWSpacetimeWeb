5d2869c93431e2b8d6b7856cf1511ee5630c3556
Andre Lino <andre.cesar97@hotmail.com>
2023-05-26 16:21:03 -0300

    ajustes do front

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -14,7 +14,10 @@ export function Profile() {
       />
       <p className="max-w-[140px] text-sm leading-snug transition-colors hover:text-gray-50">
         {name}
-        <a href="#" className="block text-red-400 hover:text-red-300">
+        <a
+          href="/api/auth/logout"
+          className="block text-red-400 hover:text-red-300"
+        >
           Quero sair
         </a>
       </p>
